bdd9ca02ddf31ffff4d472fd1d073fd9513e450b
Manoj Kumar <ms8460149@gmail.com>
2024-11-08 08:31:11 +0530

    feat(logo): add logo and favicon

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Pagnination from "./Pagnination";
 const STORIES_ENDPOINT =
   "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty";
@@ -6,9 +7,16 @@ export default async function Home() {
   const storyIds = await res.json();
   return (
     <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] max-w-5xl mx-auto">
-      <h1 className="text-orange-500 text-3xl font-semibold mb-4">
+      <div className="text-orange-500 text-3xl font-semibold mb-4 flex items-center gap-1">
+        <Image
+          src={"/hot-dog.png"}
+          width={50}
+          height={50}
+          alt="hot-dog"
+          className="object-contain"
+        />
         Hacker News
-      </h1>
+      </div>
       <Pagnination storyIds={storyIds || []} />
     </div>
   );
